7f0d11ab9f5a7feeb5d37825faff9e1098635614
Sofiya <ssantrvit@gmail.com>
2023-06-11 23:52:20 +0300

    Homework 9

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void AllNumbers(int number) {
+    if (number == 0) {
+        return;
+    }
+    Console. Write($"{number} ");
+    AllNumbers(number-1);
+}
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 1) {
+    Console.WriteLine("Исходное число должно быть больше, либо равно 1. Пустой вывод");
+}
+else {
+    AllNumbers(N);
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+void SummMN(int M, int N, int Summ) {
+    if (M > N) {
+    Console.WriteLine($"Сумма равна {Summ}.");
+    return;
+    }
+    else {
+        Summ += (M++);
+
+    }
+   SummMN(M, N, Summ);
+}
+
+Console.Write("Введите M (меньшее число): ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите M (большее число): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if ((M < 0) | (N < 0)) {
+    Console.WriteLine("Числа должны быть больше 0. Попробуйте еще раз");
+}
+else {
+if ((M > N) | (M == N)) {
+    Console.WriteLine("M должно быть меньше N. Попробуйте еще раз");
+}
+else {
+SummMN(M, N, 0);
+}
+}
+
+
